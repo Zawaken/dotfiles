08d286c50756740ee5118ac07c01fabbd0806711
Zawaken <zawaken@outlook.com>
2018-09-26 16:12:50 +0200

    made olkborder.sh remove olkborders file after executing

--- Linux/olkborder.sh
+++ Linux/olkborder.sh
@@ -6,3 +6,4 @@ ordernumber="cat olkborders | grep -oh 100005888"
 echo "" > olkborders
 wget -q https://raw.githubusercontent.com/olkb/orders/master/README.md -O olkborders
 echo -e "Your ordernumber: \"$(cat olkborders | grep -oh 100005888)\" is number${RED}$(cat olkborders | grep 100005888 | cut -c 1-4)${NC} in the order queue."
+rm olkborders
