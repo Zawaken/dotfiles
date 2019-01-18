7a98dc93571c67be8fe2d7517941c5fe8fa18498
Zawaken <zawaken@outlook.com>
2019-01-18 10:15:53 +0100

    Update olkborder.sh

--- Linux/bin/olkborder.sh
+++ Linux/bin/olkborder.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# wget -q https://raw.githubusercontent.com/olkb/orders/master/README.md -O - | grep 100005888
 RED='\033[0;31m'
 NC='\033[0m'
-ordernumber="cat olkborders | grep -oh 100005888"
+ordernumber="CHANGEME"
 echo "" > olkborders
 wget -q https://raw.githubusercontent.com/olkb/orders/master/README.md -O olkborders
-echo -e "Your ordernumber: \"$(cat olkborders | grep -oh 100005888)\" is number${RED}$(cat olkborders | grep 100005888 | cut -c 1-4)${NC} in the order queue."
+echo -e "This page was last updated \"$(cat olkborders | grep "This page was last generated" | cut -c 34-64)\""
+echo -e "Your ordernumber: \"$(cat olkborders | grep -oh ${ordernumber})\" is number${RED}$(cat olkborders | grep ${ordernumber} | cut -c 1-4)${NC} in the order queue."
 rm olkborders
